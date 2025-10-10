5d6b3563d8eb852df8e462c22093ae91b209d96c
Thandon-To <thanadonlungta@gmail.com>
2025-10-10 22:01:01 +0700

    Update 10.c

--- Homework/5.2/10.c
+++ Homework/5.2/10.c
@@ -3,28 +3,36 @@
 int main()
 {
     int score = 0;
+    
     printf("Please enter your score: ");
     scanf("%d", &score);
-    printf("Grade: ");
-    if (score >= 80)
-    {
-        printf("A\n");
+
+    if (score >= 80) {
+        printf("A!\n");
     }
-    else if (score >= 70 && score < 80)
-    {
-        printf("B\n");
+    else if (score >= 75) {
+        printf("B+!\n");
     }
-    else if (score >= 60 && score < 70)
-    {
-        printf("C\n");
+    else if (score >= 70) {
+        printf("B!\n");
     }
-    else if (score >= 50 && score < 60)
-    {
-        printf("D\n");
+    else if (score >= 65) {
+        printf("C+!\n");
     }
-    else
-    {
-        printf("F\n");
+    else if (score >= 60) {
+        printf("C!\n");
+    }
+    else if (score >= 55) {
+        printf("D+!\n");
+    }
+    else if (score >= 50) {
+        printf("D!\n");
+    }
+    else if (score < 50) {
+        printf("F!\n");
+    }
+    else {
+        printf("please enter number only.\n");
     }
     return 0;
 }
