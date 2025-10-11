76dc2103172100b844800bb8144b9659b26fc0ce
Thandon-To <thanadonlungta@gmail.com>
2025-10-11 21:13:59 +0700

    Update 21.c

--- Homework/15.1/21.c
+++ Homework/15.1/21.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 
-int file(const char *filename) {
-    FILE *fp = fopen(filename, "r");
-    if (!fp) return -1;                 // เปิดไฟล์ไม่ได้
+int file(char *filename) {
+
+    FILE *fp = fopen(filename, "r");  // ให้ fp ชี้ไปยังสตรีมไฟล์ที่เปิดได้
 
     int count = 0;
-    char token[256];                    // เก็บคำละไม่เกิน 255 ตัวอักษร
-    while (fscanf(fp, "%255s", token) == 1) {
-        count++;                        // อ่านได้ 1 คำก็นับเพิ่ม
-    }
-    fclose(fp);
-    return count;
-}
+    char token[256];
 
-int main(void) {
+    while (fscanf(fp, "%s", token) == 1) {  // อ่านคำทีละคำ (กันล้น)
+        count++;
+    } //end while
+
+    fclose(fp);  // ปิดไฟล์เมื่ออ่านเสร็จ
+    return count;
+} //end function
+
+int main() {
     char fname[256];
 
     printf("Enter file name:\n");
-    scanf("%255s", fname);
-    
+    scanf("%s", fname);
+
     int total = file(fname);
-    if (total < 0) {
-        printf("Cannot open '%s'\n", fname);
-        return 0;
-    }
 
     printf("Total number of words in '%s' : %d words\n", fname, total);
     return 0;
-}
+}//end function
